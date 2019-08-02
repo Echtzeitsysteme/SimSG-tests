3596a7826c1afca380c8e915ce796e0b7408c836
arg0n1s <basti_seppl@yahoo.com>
2019-08-02 18:07:57 +0200

    + added rules and generator

---
+++ org.simsg.applications.gksimulation/src/org/simsg/applications/gksimulation/benchmark/DemoclesBenchmark.java
@@ -0,0 +1,10 @@
+package org.simsg.applications.gksimulation.benchmark;
+
+import GoldbeterKoshland.util.ModelGenerator;
+
+public class DemoclesBenchmark {
+	
+	public static void main(String[] args) {
+	}
+
+}
