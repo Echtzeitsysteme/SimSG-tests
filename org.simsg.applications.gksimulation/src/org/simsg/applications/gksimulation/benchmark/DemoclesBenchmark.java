778cbe66060de22c3508b5e4bba0d47bdd945000
arg0n1s <basti_seppl@yahoo.com>
2019-08-04 21:18:20 +0200

    + added models

--- org.simsg.applications.gksimulation/src/org/simsg/applications/gksimulation/benchmark/DemoclesBenchmark.java
+++ org.simsg.applications.gksimulation/src/org/simsg/applications/gksimulation/benchmark/DemoclesBenchmark.java
@@ -1,10 +1,27 @@
 package org.simsg.applications.gksimulation.benchmark;
 
-import GoldbeterKoshland.util.ModelGenerator;
+import org.simsg.core.simulation.Simulation;
+import org.simsg.core.simulation.SimulationConfigurator;
 
 public class DemoclesBenchmark {
 	
 	public static void main(String[] args) {
+		Utils.generateGKSimulation("default100");
+		SimulationConfigurator config = new SimulationConfigurator(); 
+		config.setModelFolder(System.getProperty("user.dir")+"/models"); 
+		config.setModel("default100"); 
+		config.setStochasticSimulation();
+		config.addSimpleTerminationCondition(1000, -1);
+		config.addObservableStatistic();
+		 
+		Simulation sim = config.createSimulation(); 
+		sim.initialize(); 
+		sim.run(); 
+		sim.printCurrentMatches(); 
+		sim.displayResults();
+		sim.finish();
+		
+		
 	}
 
 }
