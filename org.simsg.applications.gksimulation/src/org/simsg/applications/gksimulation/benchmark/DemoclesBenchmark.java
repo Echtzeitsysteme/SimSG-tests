f12c9486aa87c9419c03e3106904cdf79d51f721
Sebastian E <sebastian.ehmes@gmail.com>
2019-08-05 17:59:40 +0200

    + added some convenience functions

--- org.simsg.applications.gksimulation/src/org/simsg/applications/gksimulation/benchmark/DemoclesBenchmark.java
+++ org.simsg.applications.gksimulation/src/org/simsg/applications/gksimulation/benchmark/DemoclesBenchmark.java
@@ -1,32 +1,93 @@
 package org.simsg.applications.gksimulation.benchmark;
 
+import org.eclipse.emf.common.util.URI;
+import org.simsg.applications.gksimulation.api.GksimulationAPI;
+import org.simsg.applications.gksimulation.api.GksimulationApp;
+import org.simsg.applications.gksimulation.api.GksimulationDemoclesApp;
+import org.simsg.applications.gksimulation.api.GksimulationHiPEApp;
 import org.simsg.core.simulation.Simulation;
 import org.simsg.core.simulation.SimulationConfigurator;
+import org.simsg.core.utils.Runtimer;
 
 import GoldbeterKoshland.util.ModelGenerator;
 
 public class DemoclesBenchmark {
 	
 	public static void main(String[] args) {
-		//ModelGenerator.createAndSaveModel("instances/default100.xmi", 100);
-		//Utils.generateGKSimulation("default100");
+		//createModels();
+		
+		
 		SimulationConfigurator config = new SimulationConfigurator(); 
 		config.setModelFolder(System.getProperty("user.dir")+"/models"); 
-		config.setModel("default100"); 
+		config.setModel("default400");
+		config.setIBeXHiPEAsEngine();
+		config.setIBeXHiPEGT();
 		config.setStochasticSimulation();
-		//config.addSimpleTerminationCondition(20000, -1);
-		config.addSimpleTerminationCondition(-1, 20.0);
+		config.addSimpleTerminationCondition(10000, -1);
+		//config.addSimpleTerminationCondition(-1, 20.0);
 		config.addObservableStatistic();
 		 
-		Simulation sim = config.createSimulation(); 
-		sim.initialize(); 
-		sim.run(); 
+		Simulation sim = config.createSimulation();
+		sim.initializeClocked();
+		sim.runClocked();
 		sim.printCurrentMatches(); 
 		sim.displayResults();
 		sim.saveModelGraph();
 		sim.finish();
 		
+		System.out.println(Runtimer.getInstance().toString());
+
+	}
+	
+	public static void rawApplications() {
+		GksimulationApp app = new GksimulationDemoclesApp();
+		app.loadModel(URI.createFileURI("instances/default400.xmi"));
 		
+		double superTic = System.currentTimeMillis();
+		double tic = System.currentTimeMillis();
+		GksimulationAPI api = app.initAPI();
+		api.updateMatches();
+		double toc = System.currentTimeMillis();
+		System.out.println("Init: "+ (toc-tic)+"ms");
+		
+		tic = System.currentTimeMillis();
+		System.out.println(api.ktXfwd().countMatches());
+		toc = System.currentTimeMillis();
+		System.out.println("Count: "+ (toc-tic)+"ms");
+		
+		tic = System.currentTimeMillis();
+		api.ktXfwd().apply();
+		toc = System.currentTimeMillis();
+		System.out.println("Apply: "+ (toc-tic)+"ms");
+		
+		tic = System.currentTimeMillis();
+		System.out.println(api.ktXfwd().countMatches());
+		toc = System.currentTimeMillis();
+		System.out.println("Count: "+ (toc-tic)+"ms");
+		
+		tic = System.currentTimeMillis();
+		api.updateMatches();
+		System.out.println(api.ktXfwd().countMatches());
+		toc = System.currentTimeMillis();
+		System.out.println("Update: "+ (toc-tic)+"ms");
+		
+		double superToc = System.currentTimeMillis();
+		System.out.println("Total: "+ (superToc-superTic)+"ms");
+		
+		api.terminate();
+	}
+	
+	public static void createModels() {
+		ModelGenerator.createAndSaveModel("instances/default100.xmi", 100);
+		Utils.generateGKSimulation("default100");
+		ModelGenerator.createAndSaveModel("instances/default200.xmi", 200);
+		Utils.generateGKSimulation("default200");
+		ModelGenerator.createAndSaveModel("instances/default400.xmi", 400);
+		Utils.generateGKSimulation("default400");
+		ModelGenerator.createAndSaveModel("instances/default800.xmi", 800);
+		Utils.generateGKSimulation("default800");
+		ModelGenerator.createAndSaveModel("instances/default1600.xmi", 1600);
+		Utils.generateGKSimulation("default1600");
 	}
 
 }
