65e8c8a53d27541c0bd3fd0c6b5d62438377920c
SebastianE <sebastian.ehmes@gmail.com>
2020-11-20 15:53:30 +0100

    fixed polymer rules by adding additional NACs

--- Polymers/src/polymers/simulation/SimulationRunner.java
+++ Polymers/src/polymers/simulation/SimulationRunner.java
@@ -11,8 +11,8 @@ public class SimulationRunner {
 
 	public static void main(String[] args) {
 		RulesSimSGApi api = new RulesSimSGApi();
-		api.configureForHiPE();
-//		api.configureForDemocles();
+//		api.configureForHiPE();
+		api.configureForDemocles();
 		api.configureStochasticSimulation();
 		SimulationConfigurator config = api.getSimulationConfigurator();
 		config.setModel("10atoms_unconnected");
@@ -24,20 +24,12 @@ public class SimulationRunner {
 //		sim.displayResults();
 //		sim.finish();
 		
-		SimulationContainer simContainer = config.createSimulations(1);
+		SimulationContainer simContainer = config.createSimulations(10);
 		simContainer.initialize();
 		simContainer.run();
 		simContainer.displayResults();
 		simContainer.finish();
-		
-//		Simulation sim = config.createSimulation();
-//		sim.initialize();
-//		Simulation sim1 = config.createSimulation();
-//		sim1.initialize();
-//		Simulation sim2 = config.createSimulation();
-//		sim2.initialize();
-//		Simulation sim3 = config.createSimulation();
-//		sim3.initialize();
+
 	}
 
 }
