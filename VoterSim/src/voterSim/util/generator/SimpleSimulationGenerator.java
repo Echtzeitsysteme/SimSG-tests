b7650f62348c7ed1838f65d0e181b11a9fc9e3ff
SebastianE <sebastian.ehmes@gmail.com>
2020-11-25 11:26:29 +0100

    added simple voter model

---
+++ VoterSim/src/voterSim/util/generator/SimpleSimulationGenerator.java
@@ -0,0 +1,24 @@
+package voterSim.util.generator;
+
+import org.eclipse.emf.common.util.URI;
+import org.simsg.simulationdefinition.utils.SimulationDefinitionGenerator;
+
+import util.generator.VoterGenerator;
+
+public class SimpleSimulationGenerator {
+	
+	public static void main(String[] args) {
+		VoterGenerator votersGen = new VoterGenerator();
+		votersGen.setNumOfVoters(3);
+		votersGen.generate(System.getProperty("user.dir")+"/instances/simulation_instances/3voters_unconnected.xmi");
+		
+		SimulationDefinitionGenerator gen = new SimulationDefinitionGenerator("3voters_unconnected");
+		gen.setIBeXModel(URI.createFileURI(System.getProperty("user.dir")+"/src-gen/voterSim/rules/api/ibex-patterns.xmi"));
+		gen.setModelURI(System.getProperty("user.dir")+"/instances/simulation_instances/"+"3voters_unconnected"+".xmi"); 
+		gen.addPatternObservation("disagree");
+		gen.addPatternObservation("agree");
+		gen.addPatternObservation("newVoter");
+		gen.addTerminationConditionIterations(100);
+		gen.saveDefinition(System.getProperty("user.dir")+"/instances/simulation_definitions/"+"3voters_unconnected"+".xmi");
+	}
+}
